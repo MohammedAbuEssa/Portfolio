d6c5b378d016942c68273c6fd7baba695ff999af
MohammedAbuEssa <mabuissa5@gmail.com>
2023-10-22 12:15:54 +0000

    First push

---
+++ src/data/projects.js
@@ -0,0 +1,33 @@
+// Import images
+import WebImage1 from "../images/Web-Project-1.png";
+import WebImage2 from "../images/Web project 2/1.jpg";
+import WebImage3 from "../images/Screenshot 2023-10-21 215613.png";
+
+export const projectsData = [
+  {
+    id: 1,
+    title: "JoinMe Application",
+    category: "Web Application",
+    img: WebImage1,
+    ProjectHeader: {
+      title: "React social media website",
+      publishDate: "23/12/2022",
+      tags: "UI / Frontend / Backend",
+    },
+    github: "https://github.com/MohammedAbuEssa/JoinMe",
+  },
+  {
+    id: 2,
+    title: "Learning Management System for schools",
+    category: "Web Application",
+    img: WebImage2,
+    github: "https://github.com/LM-System/frontend-lms",
+  },
+  {
+    id: 3,
+    title: "Learning Management System for institution",
+    category: "Web Application",
+    img: WebImage3,
+    github: "https://github.com/LM-System/frontend",
+  },
+];
